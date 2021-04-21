807d6dce5af903944cf4357050f8851c8de82323
Vlad Ricean <61254142+IoanRicean@users.noreply.github.com>
2021-04-21 14:14:08 +0300

    am adaugat spinner dep

--- app/src/main/java/com/example/badgeapppc/FirstActivity.java
+++ app/src/main/java/com/example/badgeapppc/FirstActivity.java
@@ -3,6 +3,8 @@ package com.example.badgeapppc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinnerAlegeDepartament);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }
